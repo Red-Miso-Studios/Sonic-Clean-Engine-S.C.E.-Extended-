1dc228d561f7eca68f04a61b101c8830ed7e2956
TheBlad768 <master.metalsonic2014@gmail.com>
2025-03-11 19:37:13 +0400

    New style of source code.
    - All files are sorted by the new
    - Removed useless lua

---
+++ Data/Kosinski Plus Module Data.asm
@@ -0,0 +1,133 @@
+; ===========================================================================
+; Kosinski Plus Module compressed Renders graphics
+; ===========================================================================
+
+ArtKosPM_HUD:					binclude "Objects/Renders/HUD/KosinskiPM Art/HUD.kospm"
+	even
+ArtKosPM_SonicLifeIcon:			binclude "Objects/Renders/HUD/KosinskiPM Art/Sonic Life Icon.kospm"
+	even
+ArtKosPM_MilesLifeIcon:			binclude "Objects/Renders/HUD/KosinskiPM Art/Miles Life Icon.kospm"
+	even
+ArtKosPM_TailsLifeIcon:			binclude "Objects/Renders/HUD/KosinskiPM Art/Tails Life Icon.kospm"
+	even
+ArtKosPM_KnucklesLifeIcon:		binclude "Objects/Renders/HUD/KosinskiPM Art/Knuckles Life Icon.kospm"
+	even
+
+; ===========================================================================
+; Kosinski Plus Module compressed Main graphics
+; ===========================================================================
+
+ArtKosPM_Ring_Sparks:			binclude "Objects/Main/Rings/KosinskiPM Art/Sparks.kospm"
+	even
+ArtKosPM_Monitors:				binclude "Objects/Main/Monitor/KosinskiPM Art/Monitors.kospm"
+	even
+ArtKosPM_SpikesSprings:			binclude "Objects/Main/Spikes/KosinskiPM Art/SpikesSprings.kospm"
+	even
+ArtKosPM_BossExplosion:			binclude "Objects/Main/Explosion/KosinskiPM Art/Boss Explosion.kospm"
+	even
+ArtKosPM_Explosion:				binclude "Objects/Main/Explosion/KosinskiPM Art/Explosion.kospm"
+	even
+ArtKosPM_EnemyPtsStarPost:		binclude "Objects/Main/StarPost/KosinskiPM Art/Enemy points.kospm"
+	even
+ArtKosPM_GameOver:				binclude "Objects/Main/Game Over/KosinskiPM Art/Game Over.kospm"
+	even
+ArtKosPM_GrayButton:			binclude "Objects/Main/Button/KosinskiPM Art/Gray Button.kospm"
+	even
+ArtKosPM_DiagonalSpring:		binclude "Objects/Main/Spring/KosinskiPM Art/Diagonal Spring.kospm"
+	even
+ArtKosPM_Bubbles:				binclude "Objects/Main/Bubbler/KosinskiPM Art/Bubbles.kospm"
+	even
+ArtKosPM_WaveSplash:			binclude "Objects/Main/Wave Splash/KosinskiPM Art/Wave Splash.kospm"
+	even
+ArtKosPM_EggCapsule:			binclude "Objects/Main/Egg Capsule/KosinskiPM Art/Egg Capsule.kospm"
+	even
+ArtKosPM_Seal:					binclude "Objects/Main/Animals/KosinskiPM Art/Seal.kospm"
+	even
+ArtKosPM_Pig:					binclude "Objects/Main/Animals/KosinskiPM Art/Pig.kospm"
+	even
+ArtKosPM_BlueFlicky:				binclude "Objects/Main/Animals/KosinskiPM Art/Blue Flicky.kospm"
+	even
+ArtKosPM_Chicken:				binclude "Objects/Main/Animals/KosinskiPM Art/Chicken.kospm"
+	even
+ArtKosPM_Penguin:				binclude "Objects/Main/Animals/KosinskiPM Art/Penguin.kospm"
+	even
+ArtKosPM_Squirrel:				binclude "Objects/Main/Animals/KosinskiPM Art/Squirrel.kospm"
+	even
+ArtKosPM_Rabbit:				binclude "Objects/Main/Animals/KosinskiPM Art/Rabbit.kospm"
+	even
+ArtKosPM_ResultsGeneral:			binclude "Objects/Main/Results/KosinskiPM Art/General.kospm"
+	even
+ArtKosPM_ResultsSONIC:			binclude "Objects/Main/Results/KosinskiPM Art/SONIC.kospm"
+	even
+ArtKosPM_ResultsMILES:			binclude "Objects/Main/Results/KosinskiPM Art/MILES.kospm"
+	even
+ArtKosPM_ResultsTAILS:			binclude "Objects/Main/Results/KosinskiPM Art/TAILS.kospm"
+	even
+ArtKosPM_ResultsKNUCKLES:		binclude "Objects/Main/Results/KosinskiPM Art/KNUCKLES.kospm"
+	even
+ArtKosPM_StarPostStars1:			binclude "Objects/Main/StarPost/KosinskiPM Art/StarPost Stars 1.kospm"
+	even
+ArtKosPM_StarPostStars2:			binclude "Objects/Main/StarPost/KosinskiPM Art/StarPost Stars 2.kospm"
+	even
+ArtKosPM_StarPostStars3:			binclude "Objects/Main/StarPost/KosinskiPM Art/StarPost Stars 3.kospm"
+	even
+
+; ===========================================================================
+; Kosinski Plus Module compressed Title Card graphics
+; ===========================================================================
+
+ArtKosPM_TitleCardNum1:			binclude "Objects/Main/Title Card/KosinskiPM Art/Title Card Number 1.kospm"
+	even
+ArtKosPM_TitleCardNum2:		binclude "Objects/Main/Title Card/KosinskiPM Art/Title Card Number 2.kospm"
+	even
+ArtKosPM_TitleCardNum3:		binclude "Objects/Main/Title Card/KosinskiPM Art/Title Card Number 3.kospm"
+	even
+ArtKosPM_TitleCardNum4:		binclude "Objects/Main/Title Card/KosinskiPM Art/Title Card Number 4.kospm"
+	even
+ArtKosPM_TitleCardRedAct:		binclude "Objects/Main/Title Card/KosinskiPM Art/Title Card Red ACT.kospm"
+	even
+ArtKosPM_DEZTitleCard:			binclude "Objects/Main/Title Card/KosinskiPM Art/Levels/DEZ - Title Card.kospm"
+	even
+
+; ===========================================================================
+; Kosinski Plus Module compressed Level Select screen graphics
+; ===========================================================================
+
+ArtKosPM_LevelSelectText:			binclude "Screens/Level Select/KosinskiPM Art/Text.kospm"
+	even
+
+; ===========================================================================
+; Kosinski Plus Module compressed Continue screen graphics
+; ===========================================================================
+
+ArtKosPM_ContinueSprites:		binclude "Screens/Continue/KosinskiPM Art/Player Sprites.kospm"
+	even
+ArtKosPM_ContinueIcons:			binclude "Screens/Continue/KosinskiPM Art/Player Icons.kospm"
+	even
+ArtKosPM_ContinueDigits:			binclude "Screens/Continue/KosinskiPM Art/Digits.kospm"
+	even
+ArtKosPM_EggRoboBadnik:		binclude "Screens/Continue/KosinskiPM Art/Egg Robo Badnik.kospm"
+	even
+ArtKosPM_LargeTextCredits:		binclude "Screens/Continue/KosinskiPM Art/Large Text.kospm"
+	even
+
+; ===========================================================================
+; Kosinski Plus Module compressed Level screen graphics (Environment)
+; ===========================================================================
+
+
+; ===========================================================================
+; Kosinski Plus Module compressed Level screen graphics (Bosses)
+; ===========================================================================
+
+ArtKosPM_RobotnikShip:			binclude "Objects/Bosses/Robotnik/KosinskiPM Art/Robotnik Ship.kospm"
+	even
+ArtKosPM_EggRoboHead:			binclude "Objects/Bosses/Robotnik/KosinskiPM Art/Egg Robo Head.kospm"
+	even
+
+; ===========================================================================
+; Kosinski Plus Module compressed Level screen graphics (Enemies)
+; ===========================================================================
+
+ArtKosPM_Spikebonker:			binclude "Objects/Enemies/Spikebonker/KosinskiPM Art/Spikebonker.kospm"
+	even
---
+++ Objects/Main/Game Over/Game Over.asm
@@ -0,0 +1,91 @@
+; ---------------------------------------------------------------------------
+; Game Over (Object)
+; ---------------------------------------------------------------------------
+
+; =============== S U B R O U T I N E =======================================
+
+Obj_GameOver:
+
+		; wait for KosPlusM queue to clear
+		tst.w	(KosPlus_modules_left).w
+		beq.s	.endplc
+		rts
+; ---------------------------------------------------------------------------
+
+.endplc
+		clr.w	priority(a0)
+		move.l	#Map_GameOver,mappings(a0)
+		move.w	#make_art_tile(ArtTile_Shield,0,1),art_tile(a0)
+		move.b	#rfMulti,render_flags(a0)							; set multi-draw flag
+		move.l	#.xpos,address(a0)
+
+		; GAME/TIME (frame)
+		move.w	#$80-48,x_pos(a0)
+		move.w	#$80+(224/2),y_pos(a0)
+
+		; OVER (frame)
+		lea	sub2_x_pos(a0),a1
+		move.w	#$80+(320+48),(a1)+
+		move.w	y_pos(a0),(a1)+
+		moveq	#1,d0
+		move.w	d0,mainspr_childsprites(a0)						; number of child sprites
+		add.b	mapping_frame(a0),d0
+		move.w	d0,(a1)
+
+.xpos
+		lea	sub2_x_pos(a0),a1
+
+		; check center
+		move.w	x_pos(a0),d0
+		sub.w	(a1),d0											; is center position xpos?
+		beq.s	.settime											; if yes, branch
+
+		; move sprites
+		moveq	#16,d1
+		add.w	d1,x_pos(a0)
+		sub.w	d1,(a1)
+
+		; draw
+		bra.s	.draw
+; ---------------------------------------------------------------------------
+
+.settime
+		move.w	#9*60,anim_frame_timer(a0)
+		move.l	#.wait,address(a0)
+
+.wait
+		move.b	(Ctrl_1_pressed).w,d0
+		or.b	(Ctrl_2_pressed).w,d0
+		andi.b	#btnABCS,d0										; is A/B/C/Start pressed?
+		bne.s	.end												; if yes, branch
+
+		; wait
+		tst.w	anim_frame_timer(a0)
+		beq.s	.end
+		subq.w	#1,anim_frame_timer(a0)
+
+		; draw
+		bra.s	.draw
+; ---------------------------------------------------------------------------
+
+.end
+		tst.b	(Time_over_flag).w
+		bne.s	.restart
+		lea	4*3(sp),sp											; exit from object and current screen
+		move.b	#GameModeID_ContinueScreen,(Game_mode).w		; load continue screen
+		tst.b	(Continue_count).w
+		bne.s	.draw
+		move.b	#GameModeID_LevelSelectScreen,(Game_mode).w	; load level select screen
+		bra.s	.draw
+; ---------------------------------------------------------------------------
+
+.restart
+		clr.l	(Saved_timer).w
+		st	(Restart_level_flag).w
+
+.draw
+		clr.w	(Collision_response_list).w							; reset collision response list
+		jmp	(Draw_Sprite).w
+; ---------------------------------------------------------------------------
+
+		include "Objects/Main/Game Over/Object Data/Map - Game Over.asm"
