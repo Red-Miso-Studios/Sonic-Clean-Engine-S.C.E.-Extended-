6e9254d55c9561efa331335b1572ce056818ba1d
TheBlad768 <master.metalsonic2014@gmail.com>
2025-03-11 19:51:06 +0400

    New style of source code.
    - All files are sorted by the new
    - Removed useless lua

---
+++ Data/Palette Data.asm
@@ -0,0 +1,46 @@
+; ===========================================================================
+; Palette Main data
+; ===========================================================================
+
+Pal_Sonic:							bincludeEntry "Objects/Players/Sonic/Palettes/Sonic.pal"
+	even
+Pal_WaterSonic:						bincludeEntry "Objects/Players/Sonic/Palettes/Water Sonic.pal"
+	even
+Pal_Knuckles:						bincludeEntry "Objects/Players/Knuckles/Palettes/Knuckles.pal"
+	even
+Pal_WaterKnuckles:					bincludeEntry "Objects/Players/Knuckles/Palettes/Water Knuckles.pal"
+	even
+Pal_LevelSelect:						bincludeEntry "Screens/Level Select/Palettes/1.pal"
+	even
+Pal_Continue:						bincludeEntry "Screens/Continue/Palettes/1.pal"
+	even
+Pal_DEZ:							bincludeEntry "Levels/DEZ/Palettes/Death Egg Zone.pal"
+	even
+Pal_WaterDEZ:						bincludeEntry "Levels/DEZ/Palettes/Water Death Egg Zone.pal"
+	even
+
+; ===========================================================================
+; Animated palette player data
+; ===========================================================================
+
+PalCycle_SuperSonic:					bincludeEntry "Objects/Players/Sonic/Palettes/Animated/Super.pal"
+	even
+PalCycle_HyperSonic:					bincludeEntry "Objects/Players/Sonic/Palettes/Animated/Hyper.pal"
+	even
+PalCycle_SuperSonicUnderwater:		bincludeEntry "Objects/Players/Sonic/Palettes/Animated/Underwater.pal"
+	even
+PalCycle_SuperTails:					bincludeEntry "Objects/Players/Tails/Palettes/Animated/Super.pal"
+	even
+PalCycle_SuperHyperKnuckles:			bincludeEntry "Objects/Players/Knuckles/Palettes/Animated/Super and Hyper.pal"
+	even
+PalCycle_SuperHyperKnucklesRevert:	bincludeEntry "Objects/Players/Knuckles/Palettes/Animated/Revert.pal"
+	even
+
+; ===========================================================================
+; Animated palette Level screen data
+; ===========================================================================
+
+AnPal_PalDEZ12_1:					bincludeEntry "Levels/DEZ/Palettes/Animated/Palettes/1.pal"
+	even
+AnPal_PalDEZ12_2:					bincludeEntry "Levels/DEZ/Palettes/Animated/Palettes/2.pal"
+	even
