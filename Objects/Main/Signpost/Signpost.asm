dfca912f135225c2b06349e76aad9a37f87cf40c
TheBlad768 <master.metalsonic2014@gmail.com>
2025-05-18 15:54:15 +0400

    DPLC and Mapping swap

--- Objects/Main/Signpost/Signpost.asm
+++ Objects/Main/Signpost/Signpost.asm
@@ -387,6 +387,6 @@ AniRaw_SignpostSparkle:	dc.b 1, 1, 2, 3, 4, arfEnd
 	even
 ; ---------------------------------------------------------------------------
 
-		include "Objects/Main/Signpost/Object Data/DPLC - End Signs.asm"
 		include "Objects/Main/Signpost/Object Data/Map - End Signs.asm"
+		include "Objects/Main/Signpost/Object Data/DPLC - End Signs.asm"
 		include "Objects/Main/Signpost/Object Data/Map - Signpost Stub.asm"
