4d234695c80afdd4e1fcd5fc1a11ab5151223250
TheBlad768 <master.metalsonic2014@gmail.com>
2025-07-11 21:00:20 +0400

    Update Pause Game.asm

--- Engine/Core/Pause Game.asm
+++ Engine/Core/Pause Game.asm
@@ -55,6 +55,6 @@ Pause_Game:
 	if GameDebug
 .frameadvance
 		st	(Game_paused).w
-		SMPS_UnpauseMusic
-		rts
+		SMPS_UnpauseMusic							; unpause the music
+		rts											; advance by a single frame
 	endif
