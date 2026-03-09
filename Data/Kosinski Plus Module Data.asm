d57f0e92074bee699aefb3c7458e4babb6eccb48
TheBlad768 <master.metalsonic2014@gmail.com>
2026-03-09 23:30:40 +0400

    incfile macro update

--- Data/Kosinski Plus Module Data.asm
+++ Data/Kosinski Plus Module Data.asm
@@ -4,12 +4,12 @@
 
 ;		Attribute	| Filename	| Folder
 
-		incfile.b	ArtKosPM_HUD, "Objects/Renders/HUD/KosinskiPM Art/HUD.kospm"
-		incfile.b	ArtKosPM_SonicLifeIcon, "Objects/Renders/HUD/KosinskiPM Art/Sonic Life Icon.kospm"
-		incfile.b	ArtKosPM_MilesLifeIcon, "Objects/Renders/HUD/KosinskiPM Art/Miles Life Icon.kospm"
-		incfile.b	ArtKosPM_TailsLifeIcon, "Objects/Renders/HUD/KosinskiPM Art/Tails Life Icon.kospm"
-		incfile.b	ArtKosPM_KnucklesLifeIcon, "Objects/Renders/HUD/KosinskiPM Art/Knuckles Life Icon.kospm"
-		incfile.b	ArtKosPM_GameOver, "Objects/Renders/Game Over/KosinskiPM Art/Game Over.kospm"
+		incfile.ba	ArtKosPM_HUD, "Objects/Renders/HUD/KosinskiPM Art/HUD.kospm"
+		incfile.ba	ArtKosPM_SonicLifeIcon, "Objects/Renders/HUD/KosinskiPM Art/Sonic Life Icon.kospm"
+		incfile.ba	ArtKosPM_MilesLifeIcon, "Objects/Renders/HUD/KosinskiPM Art/Miles Life Icon.kospm"
+		incfile.ba	ArtKosPM_TailsLifeIcon, "Objects/Renders/HUD/KosinskiPM Art/Tails Life Icon.kospm"
+		incfile.ba	ArtKosPM_KnucklesLifeIcon, "Objects/Renders/HUD/KosinskiPM Art/Knuckles Life Icon.kospm"
+		incfile.ba	ArtKosPM_GameOver, "Objects/Renders/Game Over/KosinskiPM Art/Game Over.kospm"
 
 ; ===========================================================================
 ; Kosinski Plus Module compressed Main graphics
@@ -17,32 +17,32 @@
 
 ;		Attribute	| Filename	| Folder
 
-		incfile.b	ArtKosPM_Ring_Sparks, "Objects/Main/Rings/KosinskiPM Art/Sparks.kospm"
-		incfile.b	ArtKosPM_Monitors, "Objects/Main/Monitor/KosinskiPM Art/Monitors.kospm"
-		incfile.b	ArtKosPM_SpikesSprings, "Objects/Main/Spikes/KosinskiPM Art/SpikesSprings.kospm"
-		incfile.b	ArtKosPM_BossExplosion, "Objects/Main/Explosion/KosinskiPM Art/Boss Explosion.kospm"
-		incfile.b	ArtKosPM_Explosion, "Objects/Main/Explosion/KosinskiPM Art/Explosion.kospm"
-		incfile.b	ArtKosPM_EnemyPtsStarPost, "Objects/Main/StarPost/KosinskiPM Art/Enemy points.kospm"
-		incfile.b	ArtKosPM_GrayButton, "Objects/Main/Button/KosinskiPM Art/Gray Button.kospm"
-		incfile.b	ArtKosPM_DiagonalSpring, "Objects/Main/Spring/KosinskiPM Art/Diagonal Spring.kospm"
-		incfile.b	ArtKosPM_Bubbles, "Objects/Main/Bubbler/KosinskiPM Art/Bubbles.kospm"
-		incfile.b	ArtKosPM_WaveSplash, "Objects/Main/Wave Splash/KosinskiPM Art/Wave Splash.kospm"
-		incfile.b	ArtKosPM_EggCapsule, "Objects/Main/Egg Capsule/KosinskiPM Art/Egg Capsule.kospm"
-		incfile.b	ArtKosPM_Seal, "Objects/Main/Animals/KosinskiPM Art/Seal.kospm"
-		incfile.b	ArtKosPM_Pig, "Objects/Main/Animals/KosinskiPM Art/Pig.kospm"
-		incfile.b	ArtKosPM_BlueFlicky, "Objects/Main/Animals/KosinskiPM Art/Blue Flicky.kospm"
-		incfile.b	ArtKosPM_Chicken, "Objects/Main/Animals/KosinskiPM Art/Chicken.kospm"
-		incfile.b	ArtKosPM_Penguin, "Objects/Main/Animals/KosinskiPM Art/Penguin.kospm"
-		incfile.b	ArtKosPM_Squirrel, "Objects/Main/Animals/KosinskiPM Art/Squirrel.kospm"
-		incfile.b	ArtKosPM_Rabbit, "Objects/Main/Animals/KosinskiPM Art/Rabbit.kospm"
-		incfile.b	ArtKosPM_ResultsGeneral, "Objects/Main/Results/KosinskiPM Art/General.kospm"
-		incfile.b	ArtKosPM_ResultsSONIC, "Objects/Main/Results/KosinskiPM Art/SONIC.kospm"
-		incfile.b	ArtKosPM_ResultsMILES, "Objects/Main/Results/KosinskiPM Art/MILES.kospm"
-		incfile.b	ArtKosPM_ResultsTAILS, "Objects/Main/Results/KosinskiPM Art/TAILS.kospm"
-		incfile.b	ArtKosPM_ResultsKNUCKLES, "Objects/Main/Results/KosinskiPM Art/KNUCKLES.kospm"
-		incfile.b	ArtKosPM_StarPostStars1, "Objects/Main/StarPost/KosinskiPM Art/StarPost Stars 1.kospm"
-		incfile.b	ArtKosPM_StarPostStars2, "Objects/Main/StarPost/KosinskiPM Art/StarPost Stars 2.kospm"
-		incfile.b	ArtKosPM_StarPostStars3, "Objects/Main/StarPost/KosinskiPM Art/StarPost Stars 3.kospm"
+		incfile.ba	ArtKosPM_Ring_Sparks, "Objects/Main/Rings/KosinskiPM Art/Sparks.kospm"
+		incfile.ba	ArtKosPM_Monitors, "Objects/Main/Monitor/KosinskiPM Art/Monitors.kospm"
+		incfile.ba	ArtKosPM_SpikesSprings, "Objects/Main/Spikes/KosinskiPM Art/SpikesSprings.kospm"
+		incfile.ba	ArtKosPM_BossExplosion, "Objects/Main/Explosion/KosinskiPM Art/Boss Explosion.kospm"
+		incfile.ba	ArtKosPM_Explosion, "Objects/Main/Explosion/KosinskiPM Art/Explosion.kospm"
+		incfile.ba	ArtKosPM_EnemyPtsStarPost, "Objects/Main/StarPost/KosinskiPM Art/Enemy points.kospm"
+		incfile.ba	ArtKosPM_GrayButton, "Objects/Main/Button/KosinskiPM Art/Gray Button.kospm"
+		incfile.ba	ArtKosPM_DiagonalSpring, "Objects/Main/Spring/KosinskiPM Art/Diagonal Spring.kospm"
+		incfile.ba	ArtKosPM_Bubbles, "Objects/Main/Bubbler/KosinskiPM Art/Bubbles.kospm"
+		incfile.ba	ArtKosPM_WaveSplash, "Objects/Main/Wave Splash/KosinskiPM Art/Wave Splash.kospm"
+		incfile.ba	ArtKosPM_EggCapsule, "Objects/Main/Egg Capsule/KosinskiPM Art/Egg Capsule.kospm"
+		incfile.ba	ArtKosPM_Seal, "Objects/Main/Animals/KosinskiPM Art/Seal.kospm"
+		incfile.ba	ArtKosPM_Pig, "Objects/Main/Animals/KosinskiPM Art/Pig.kospm"
+		incfile.ba	ArtKosPM_BlueFlicky, "Objects/Main/Animals/KosinskiPM Art/Blue Flicky.kospm"
+		incfile.ba	ArtKosPM_Chicken, "Objects/Main/Animals/KosinskiPM Art/Chicken.kospm"
+		incfile.ba	ArtKosPM_Penguin, "Objects/Main/Animals/KosinskiPM Art/Penguin.kospm"
+		incfile.ba	ArtKosPM_Squirrel, "Objects/Main/Animals/KosinskiPM Art/Squirrel.kospm"
+		incfile.ba	ArtKosPM_Rabbit, "Objects/Main/Animals/KosinskiPM Art/Rabbit.kospm"
+		incfile.ba	ArtKosPM_ResultsGeneral, "Objects/Main/Results/KosinskiPM Art/General.kospm"
+		incfile.ba	ArtKosPM_ResultsSONIC, "Objects/Main/Results/KosinskiPM Art/SONIC.kospm"
+		incfile.ba	ArtKosPM_ResultsMILES, "Objects/Main/Results/KosinskiPM Art/MILES.kospm"
+		incfile.ba	ArtKosPM_ResultsTAILS, "Objects/Main/Results/KosinskiPM Art/TAILS.kospm"
+		incfile.ba	ArtKosPM_ResultsKNUCKLES, "Objects/Main/Results/KosinskiPM Art/KNUCKLES.kospm"
+		incfile.ba	ArtKosPM_StarPostStars1, "Objects/Main/StarPost/KosinskiPM Art/StarPost Stars 1.kospm"
+		incfile.ba	ArtKosPM_StarPostStars2, "Objects/Main/StarPost/KosinskiPM Art/StarPost Stars 2.kospm"
+		incfile.ba	ArtKosPM_StarPostStars3, "Objects/Main/StarPost/KosinskiPM Art/StarPost Stars 3.kospm"
 
 ;		Attribute	| Filename	| Folder
 
@@ -52,11 +52,11 @@
 
 ;		Attribute	| Filename	| Folder
 
-		incfile.b	ArtKosPM_TitleCardRedAct, "Objects/Main/Title Card/KosinskiPM Art/Title Card Red ACT.kospm"
-		incfile.b	ArtKosPM_TitleCardNum1, "Objects/Main/Title Card/KosinskiPM Art/Title Card Number 1.kospm"
-		incfile.b	ArtKosPM_TitleCardNum2, "Objects/Main/Title Card/KosinskiPM Art/Title Card Number 2.kospm"
-		incfile.b	ArtKosPM_TitleCardNum3, "Objects/Main/Title Card/KosinskiPM Art/Title Card Number 3.kospm"
-		incfile.b	ArtKosPM_TitleCardNum4, "Objects/Main/Title Card/KosinskiPM Art/Title Card Number 4.kospm"
+		incfile.ba	ArtKosPM_TitleCardRedAct, "Objects/Main/Title Card/KosinskiPM Art/Title Card Red ACT.kospm"
+		incfile.ba	ArtKosPM_TitleCardNum1, "Objects/Main/Title Card/KosinskiPM Art/Title Card Number 1.kospm"
+		incfile.ba	ArtKosPM_TitleCardNum2, "Objects/Main/Title Card/KosinskiPM Art/Title Card Number 2.kospm"
+		incfile.ba	ArtKosPM_TitleCardNum3, "Objects/Main/Title Card/KosinskiPM Art/Title Card Number 3.kospm"
+		incfile.ba	ArtKosPM_TitleCardNum4, "Objects/Main/Title Card/KosinskiPM Art/Title Card Number 4.kospm"
 
 ; ===========================================================================
 ; Kosinski Plus Module compressed Level Select screen graphics
@@ -64,7 +64,7 @@
 
 ;		Attribute	| Filename	| Folder
 
-		incfile.b	ArtKosPM_LevelSelectText, "Screens/Level Select/KosinskiPM Art/Text.kospm"
+		incfile.ba	ArtKosPM_LevelSelectText, "Screens/Level Select/KosinskiPM Art/Text.kospm"
 
 ; ===========================================================================
 ; Kosinski Plus Module compressed Continue screen graphics
@@ -72,10 +72,10 @@
 
 ;		Attribute	| Filename	| Folder
 
-		incfile.b	ArtKosPM_ContinueSprites, "Screens/Continue/KosinskiPM Art/Player Sprites.kospm"
-		incfile.b	ArtKosPM_ContinueIcons, "Screens/Continue/KosinskiPM Art/Player Icons.kospm"
-		incfile.b	ArtKosPM_ContinueDigits, "Screens/Continue/KosinskiPM Art/Digits.kospm"
-		incfile.b	ArtKosPM_EggRoboBadnik, "Screens/Continue/KosinskiPM Art/Egg Robo Badnik.kospm"
+		incfile.ba	ArtKosPM_ContinueSprites, "Screens/Continue/KosinskiPM Art/Player Sprites.kospm"
+		incfile.ba	ArtKosPM_ContinueIcons, "Screens/Continue/KosinskiPM Art/Player Icons.kospm"
+		incfile.ba	ArtKosPM_ContinueDigits, "Screens/Continue/KosinskiPM Art/Digits.kospm"
+		incfile.ba	ArtKosPM_EggRoboBadnik, "Screens/Continue/KosinskiPM Art/Egg Robo Badnik.kospm"
 
 ; ===========================================================================
 ; Kosinski Plus Module compressed Level screen graphics (Environment)
@@ -88,13 +88,13 @@
 ; ===========================================================================
 
 ;		Attribute	| Filename	| Folder
-		incfile.b	ArtKosPM_RobotnikShip, "Objects/Bosses/Robotnik/KosinskiPM Art/Robotnik Ship.kospm"
-		incfile.b	ArtKosPM_EggRoboHead, "Objects/Bosses/Robotnik/KosinskiPM Art/Egg Robo Head.kospm"
+		incfile.ba	ArtKosPM_RobotnikShip, "Objects/Bosses/Robotnik/KosinskiPM Art/Robotnik Ship.kospm"
+		incfile.ba	ArtKosPM_EggRoboHead, "Objects/Bosses/Robotnik/KosinskiPM Art/Egg Robo Head.kospm"
 
 ; ===========================================================================
 ; Kosinski Plus Module compressed Level screen graphics (Enemies)
 ; ===========================================================================
 
 ;		Attribute	| Filename	| Folder
-		incfile.b	ArtKosPM_Spikebonker, "Objects/Enemies/Spikebonker/KosinskiPM Art/Spikebonker.kospm"
+		incfile.ba	ArtKosPM_Spikebonker, "Objects/Enemies/Spikebonker/KosinskiPM Art/Spikebonker.kospm"
 
